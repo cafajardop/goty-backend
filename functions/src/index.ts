db1e2d191c4b14a033ad00e58001513cce2602f5
cafajardop <carlosafp84@hotmail.com>
2020-03-22 17:32:46 -0500

    Ajuste Back

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,10 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require("./serviceAccountKey.json");
+import * as express from 'express';
+import * as cors from 'cors';
+
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -15,12 +18,12 @@ const db = admin.firestore();
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
     response.json({
-        mensaje: 'Hola Mundo desde Funciones de Firebase!'
+        mensaje: 'Hola Mundo desde Funciones de Firebase!!'
     });
 });
 
 export const getGOTY = functions.https.onRequest(async (request, response) => {
-    
+
     // const nombre = request.query.nombre;
     // response.json({
     //     nombre
@@ -35,3 +38,42 @@ export const getGOTY = functions.https.onRequest(async (request, response) => {
 
 });
 
+//Express
+const app = express();
+app.use(cors({ origin: true }));
+
+app.get('/goty', async (req, res) => {
+
+    const getyRef = db.collection('goty');
+    const docsSnap = await getyRef.get();
+    const juegos = docsSnap.docs.map(doc => doc.data());
+
+    res.json(juegos);
+});
+
+app.post('/goty/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const gameRef = db.collection('goty').doc(id);
+    const gameSanp = await gameRef.get();
+
+    if (!gameSanp.exists) {
+        res.status(404).json({
+            ok: false,
+            mensaje: 'No existe un juego con ese ID ' + id
+        });
+    }
+    else {
+        const antes = gameSanp.data() || { votos: 0 };
+        await gameRef.update({
+            votos: antes.votos + 1
+        });
+
+        res.json({
+            ok: true,
+            mensaje: `Gracias por tu voto a ${antes.name}`
+        });
+    }
+});
+
+export const api = functions.https.onRequest(app);
